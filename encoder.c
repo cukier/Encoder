8dad8e291ef531ce327c938b6c15cbe77179243f
cuki <mauricio@krebs.com.br>
2013-03-22 14:30:35 -0300

    A

--- encoder.c
+++ encoder.c
@@ -13,5 +13,29 @@
 #use rs232(baud=9600, xmit=PIN_C6, rcv=PIN_C7)
 #use spi(MASTER, DI=PIN_C4, DO=PIN_C5, CLK=PIN_C3, MODE=0,  BAUD=500000, BITS=16, DATA_HOLD=1)
 
+static long pulsos;
+static int zeros;
+
+#INT_EXT
+void isr_ext(void){
+	clear_interrupt(INT_EXT);
+	pulsos++;
+}
+
+#INT_EXT1
+void isr_ext1(void){
+	clear_interrupt(INT_EXT1);
+	zeros++
+}
+
 void main(void) {
+	clear_interrupt(INT_EXT);
+	clear_interrupt(INT_EXT1);
+	enable_iterrupts(INT_EXT_H2L);
+	enable_iterrupts(INT_EXT1_H2L);
+	enable_interrpts(GLOBAL);
+	while(TRUE){
+		printf()
+		delay_ms(500);
+	}
 }
