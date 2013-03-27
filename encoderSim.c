511e0d5ce0383eae1889d852212848ea183237d5
cuki <mauricio@krebs.com.br>
2013-03-27 13:07:59 -0300

    construcao simulador coluna

--- encoderSim.c
+++ encoderSim.c
@@ -20,23 +20,26 @@
 #define voltas_nylon	11.5
 #define tempo_elevacao	60
 
-static float per_elevacao;
-static int pre;
-//static long long Xtal;
-static long tmr0_reg;
+static long pulsos;
+static long tm0_config = 159; // resol. enc = 1024, 11.5 voltas nylon, 60 segundos operacao
 
 int main(void) {
-	per_elevacao = (float) voltas_nylon / tempo_elevacao;
-//	Xtal = getenv("CLOCK");
-	pre = 1;
-	do {
-		tmr0_reg = (float) 4000000 * per_elevacao / (4 * pre);
-		if (tmr0_reg > 0xFFFF) {
-			pre *= 2;
-			if (pre == 256 && tmr0_reg > 0xFFFF) {
-				tmr0_reg = 0xFFFF;
-				pre = 256;
-			}
+	output_low(PIN_B0);
+	SETUP_TIMER_0(T0_INTERNAL | T0_DIV_8);
+	while (TRUE) {
+		if (!input(avanco) || !input(reversao)) {
+			set_timer0(0);
+			while (get_timer0() < tm0_config)
+				;
+			pulsos++;
+			if (!(pulsos % 4))
+				output_high(canalA);
+			else if (!(pulsos % 2))
+				output_high(canalB);
+			else if (!(pulsos % 3))
+				output_low(canalB);
+			else
+				output_low(canalA);
 		}
-	} while (tmr0_reg > 0xFFFF);
+	}
 }
