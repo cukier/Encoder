e3df44a1251dd39178aef8d175786712a907db5e
cuki <mauricio@krebs.com.br>
2013-07-31 16:16:04 -0300

    funcionando para leitura do AD4050 apenas

--- encoder.c
+++ encoder.c
@@ -9,40 +9,22 @@
 #include "as5040.h"
 
 static long leitura;
-static int zero;
-//static long periodo, aux;
 
-//#INT_CCP1
-//void isr_ccp1(void) {
-//	clear_interrupt(INT_CCP1);
-//	periodo = CCP_1 - aux;
-//	aux = CCP_1;
-//	CCP_1 = 0;
-//}
+long readDevice(void) {
+	long saida;
+
+	output_low(SLAVE_SELECT);
+	saida = spi_xfer(0);
+	output_high(SLAVE_SELECT);
+
+	return saida;
+}
 
 void main(void) {
-	delay_ms(100);
-//	output_low(SLAVE_SELECT);
-//	setup_ccp1(CCP_CAPTURE_RE);
-//	setup_timer_1(T1_INTERNAL | T1_DIV_BY_8);
-//	enable_interrupts(INT_CCP1);
-//	enable_interrupts(GLOBAL);
-	zero = 0;
-//	clear_bus();
-//	delay_ms(100);
-	setup_encoder(clockwise | _7_bit_resolution | step_direction_mode, zero);
-//	clear_bus();
-	printf("\fProg: 7 bits zero %d", zero);
-//	delay_ms(1000);
+	output_high(SLAVE_SELECT);
 	while (TRUE) {
-		output_low(SLAVE_SELECT);
-		leitura = spi_xfer(0);
-		output_high(SLAVE_SELECT);
+		leitura = readDevice();
 		printf("\f%Lu  -  %d", leitura >> 6, (int) (leitura & 0x0006) >> 1);
-//		printf("\nPer: %Lu", periodo);
 		delay_ms(500);
-//		delay_us(2);
-//		delay_cycles(8);
-//		output_toggle(PIN_C7);
 	}
 }
