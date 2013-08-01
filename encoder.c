ace6de08317be9a3eafe5d581585988c359e55b8
cuki <mauricio@krebs.com.br>
2013-08-01 14:40:56 -0300

    contagem 1

--- encoder.c
+++ encoder.c
@@ -6,59 +6,45 @@
  * 	VD - VDD
  */
 
-#include <18F452.h>
+#include <18F45k20.h>
 
-#fuses H4,NOWDT,NOPROTECT,NOLVP
-#use delay(crystal=10MHz, clock=40MHz)
+#fuses H4
+#use delay(crystal=15MHz, clock=60MHz)
 #use rs232(baud=9600, xmit=PIN_C6, rcv=PIN_C7)
-#use spi(MASTER, DI=PIN_C4, DO=PIN_C5, CLK=PIN_C3, MODE=0,  BAUD=500000, BITS=16, DATA_HOLD=1)
 
-#define	saida_desce	PIN_B6
-#define	saida_sobe	PIN_B7
-#define canalA		PIN_B0
-#define canalZ		PIN_B1
-#define canalB		PIN_B2
+#define canalA		PIN_A4
+#define canalB		PIN_B4
+#define canalZ		PIN_B5
+#define bto_sobe	PIN_D0
+#define bto_desce	PIN_D1
 
-static signed long pulsos, min, max;
-static int zeros;
-
-#include "funcoes.h"
-#include "lcd_8b.c"
-
-#INT_EXT
-void isr_ext(void) {
-	clear_interrupt(INT_EXT);
-	if (input(canalB))
-		pulsos++;
-	else
-		pulsos--;
-	if (pulsos > max)
-		max = pulsos;
-	if (pulsos < min)
-		min = pulsos;
-}
-
-#INT_EXT1
-void isr_ext1(void) {
-	clear_interrupt(INT_EXT1);
-	pulsos = 0;
-	zeros++;
-}
+static long aux, cont, cont2;
+static short ctrl, trigSobe, trigDesce;
 
 void main(void) {
-	parar();
-	lcd_init();
-	port_b_pullups(TRUE);
-	delay_ms(100);
-	clear_interrupt(INT_EXT);
-	clear_interrupt(INT_EXT1);
-	enable_interrupts(INT_EXT_L2H);
-	enable_interrupts(INT_EXT1_L2H);
-	enable_interrupts(GLOBAL);
-	subir();
+	set_timer0(0);
+	setup_timer_0(T0_EXT_H_TO_L);
 	while (TRUE) {
-		printf(lcd, "\fP%Ld A%.2f", pulsos, (float) pulsos / max * 360);
-		printf(lcd, "\nZ%d m%Ld M%Ld", zeros, min, max);
-		delay_ms(500);
+		if (!input(bto_sobe) && input(bto_desce)) {
+			if (trigSobe) {
+				trigSobe = FALSE;
+				set_timer0(cont);
+			}
+			cont = get_timer0();
+		}
+		if (!input(bto_desce) && input(bto_sobe)) {
+			cont = cont2 - get_timer0();
+		}
+		if (input(bto_sobe) && input(bto_desce) && ctrl) {
+			set_timer0(0);
+			cont2 = cont;
+			trigSobe = TRUE;
+			trigDesce = TRUE;
+		} else
+			ctrl = TRUE;
+		if (cont != aux) {
+			aux = cont;
+			printf("\f%Lu %Lu", cont, cont2);
+		}
 	}
 }
