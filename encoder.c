e54300701537dce54a00540b923635db21306025
cuki <mauricio@krebs.com.br>
2014-02-17 12:08:28 -0300

    teste de botoes ok

--- encoder.c
+++ encoder.c
@@ -19,15 +19,18 @@
 #use rs232(baud=9600, xmit=PIN_C6, rcv=PIN_C7)
 #use i2c (slave,scl=PIN_C3,sda=PIN_C4,FORCE_HW,FAST=100000)
 
-#define canalA		PIN_A4
+#define canalA		PIN_B0
 #define canalB		PIN_B4
 #define canalZ		PIN_B5
 #define bto_sobe	PIN_D0
 #define bto_desce	PIN_D1
+#define bto_maual	PIN_A4
 
 #define debounce	100
 
-short ctrl, a, b;
+short ctrl, a, b, c;
+int cont = 0;
+char str[32];
 
 short check_bto(int bto) {
 	if (!input(bto)) {
@@ -44,16 +47,22 @@ int main(void) {
 	while (TRUE) {
 		a = check_bto(bto_sobe);
 		b = check_bto(bto_desce);
+		c = check_bto(bto_maual);
 		if (a && ctrl) {
 			ctrl = FALSE;
-			printf("\fPressionado Bto Sobe");
+			strcpy(str, "Pressionado Bto\nSobe");
 		} else if (b && ctrl) {
 			ctrl = FALSE;
-			printf("\fPressionado Bto Desce");
-		} else if (!(a || b) && !ctrl) {
+			strcpy(str, "Pressionado Bto\nDesce");
+		} else if (c && ctrl) {
+			ctrl = FALSE;
+			strcpy(str, "Pressionado Bto\nManual");
+		} else if (!(a || b || c) && !ctrl) {
 			ctrl = TRUE;
-			printf("\fBotoes Soltos");
+			strcpy(str, "Botoes Soltos\n");
 		}
+		printf("\f%s %u", str, cont++);
+		delay_ms(500);
 	}
 
 	return 0;
