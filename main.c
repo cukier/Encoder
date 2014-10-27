be989b342786bcef112df12ea664d1a8cd156da9
cuki <mauriciocukier@gmail.com>
2014-10-27 15:40:59 -0200

    decoder para encoder sick DFS60

--- main.c
+++ main.c
@@ -1,34 +1,81 @@
 /*
- * main.c
+ * master.c
  *
- *  Created on: 05/03/2014
+ *  Created on: 18/08/2014
  *      Author: cuki
  */
 
-#include <18F45k20.h>
+#include<18F25K22.h>
+#zero_ram
 
-#FUSES HS
-#use delay(crystal=16MHz, clock=64MHz)
-#use rs232(baud=9600, xmit=PIN_C6, rcv=PIN_C7)
+#include<stdlib.h>
 
-#include "variaveis.h"
-#include "interrupcoes.h"
-#include "funcoes.h"
+#fuses PLLEN,HSH
+#use delay(clock=60MHz, crystal=15MHz)
+#use rs232(baud=9600,xmit=pin_c6,rcv=pin_c7)
+
+#define canal_z		PIN_B3
+#define canal_a		PIN_B4
+#define canal_b		PIN_B5
+#define change_a	4
+#define change_b	5
+
+short dir;
+int changes, last_b;
+signed long m_pos, a_pos;
+
+#INT_EXT2
+void isr_ext2() {
+	clear_interrupt(INT_EXT2);
+	m_pos = 0;
+}
+
+#INT_RB
+void isr_rb() {
+	clear_interrupt(INT_RB);
+	delay_us(1);
+
+	changes = last_b ^ input_b();
+	last_b = input_b();
+
+	if (bit_test(changes, change_a))
+		dir = (input(canal_a) != input(canal_b));
+	else if (bit_test(changes, change_b))
+		dir = (input(canal_a) == input(canal_b));
+
+	if (dir)
+		m_pos++;
+	else
+		m_pos--;
+}
 
 int main(void) {
 
-	init_mcu();
+	setup_timer_0(T0_INTERNAL | T0_DIV_64);
+
+	clear_interrupt(INT_RB);
+	enable_interrupts(INT_RB);
+
+	clear_interrupt(INT_EXT2);
+	enable_interrupts(INT_EXT2_H2L);
+
+	enable_interrupts(GLOBAL);
+
+	last_b = input_b();
+
+	printf("Teste Encoder\n\r");
+	delay_ms(1000);
 
 	while (TRUE) {
 
-		m_cmd = trata_cmd(trata_bto(m_cmd));
-
-		if (posAux != pos) {
-			posAux = pos;
-			printf("Pos: %lu\n\r", pos);
-			printf("Dir: %u\n\r", (int) direction);
+		if (m_pos != a_pos) {
+			a_pos = m_pos;
+			printf("Pos: %4ld - Ang: %3.2f\n\r", m_pos,
+					(float) m_pos * 360 / 4092);
+			delay_ms(300);
 		}
-	}
 
+	}
 	return 0;
 }
+
--- makefile
+++ makefile
@@ -1,10 +1,22 @@
 CC=ccsc
-CFLAGS=+FH +LN -T -A -M -Z +DF +Y=9 +STDOUT
+PK2=pk2cmd
 
-all: main
+DEVICE=PIC18F25K22
+UNIT1=Encoder
+UNIT1_FILE=main
 
-main: main.c
-	$(CC) $(CFLAGS) main.c
+HFLAGS=+FH +LN -T -A -M -Z +DF +Y=9 +STDOUT +EA
+BFLAGS=+FB +LN -T -A -M -Z +DF +Y=9 +STDOUT +EA
+MFLAGS=+FM +LN -T -A -M -Z +DF +Y=9 +STDOUT +EA
+PK2FLAGS=-E -P$(DEVICE) -M -R -J -F
+
+all: $(UNIT1)
+
+$(UNIT1): $(UNIT1_FILE).c
+	$(CC) $(HFLAGS) $(UNIT1_FILE).c
+	
+Burn:
+	$(PK2) $(PK2FLAGS) $(UNIT1_FILE).hex
 
 clean:
-	rm *.cof *.err *.esym *.hex *.lst
+	rm *.cof *.err *.esym *.hex *.lst *.pjt *.STA *.sym *.tre *.MCP *.PWI *.DBK
